0e310d6ee0774f5a95783d4c6f7cc7af815a5171
yohber <yohannb215@gmail.com>
2023-08-09 16:25:19 +0200

    package.json modified

--- src/server.js
+++ src/server.js
@@ -8,7 +8,7 @@ app.use(express.static(__dirname + '/dist/heroku-deploy'));
 
 app.get('/*', function(req,res) {
  
-res.sendFile(path.join(__dirname+'/dist/mywolf/index.html'));
+res.sendFile(path.join(__dirname+'/dist/birthday/index.html'));
 });
 
 app.listen(process.env.PORT || 8080);
